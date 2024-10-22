a66c95e72f9db04eb81eadaaf45df2eb3c5b686a
Alexander Parrill <aparrill@specterops.io>
2024-10-22 16:30:47 -0400

    Color cursors

--- frontend/src/connection.ts
+++ frontend/src/connection.ts
@@ -10,7 +10,11 @@ export default class Connection {
     this.provider = new WebsocketProvider(wspath, room, this.doc);
     this.provider.awareness.setLocalStateField("user", {
       name: username,
-      color: "#ccccff",
+      color: hsv_to_rgb(
+        (this.doc.clientID % 255) / 255.0,
+        0.5,
+        1.0,
+      ),
     });
   }
 
@@ -18,4 +22,29 @@ export default class Connection {
     this.provider.destroy();
     this.doc.destroy();
   }
+}
+
+function hsv_to_rgb(h: number, s: number, v: number) {
+  const i = Math.floor(h * 6);
+  const f = h * 6 - i;
+  const p = v * (1 - s);
+  const q = v * (1 - f * s);
+  const t = v * (1 - (1 - f) * s);
+
+  let r,g,b;
+  switch (i % 6) {
+      case 0: r = v, g = t, b = p; break;
+      case 1: r = q, g = v, b = p; break;
+      case 2: r = p, g = v, b = t; break;
+      case 3: r = p, g = q, b = v; break;
+      case 4: r = t, g = p, b = v; break;
+      case 5: r = v, g = p, b = q; break;
+  }
+
+  const to_hex = (n: number) => {
+    var str = Math.round(n * 255).toString(16);
+    return str.length == 1 ? "0" + str : str;
+  }
+
+  return `#${to_hex(r!)}${to_hex(g!)}${to_hex(b!)}`
 }
